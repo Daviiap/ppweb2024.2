0a8f64b9ce7319ac5bccc13f8043537f5606fc2f
Daviiap <davii.sapiens@gmail.com>
2025-02-03 09:07:48 -0300

    feat: creating base route

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,29 @@
 import dotenv from "dotenv";
+import cors from "cors";
+import express, { Request, Response } from "express";
 
-function main() {
+async function main() {
   dotenv.config({
     path: "./.env",
   });
-  console.log(process.env.MSG);
+  const app = express();
+  const router = express.Router();
+
+  router.route("/").get((_: Request, res: Response) => {
+    res.sendStatus(200);
+  });
+
+  app.use(router);
+  app.use(express.json());
+  app.use(cors({ origin: "*" }));
+
+  app.listen(process.env.PORT);
 }
 
-main();
+main()
+  .then(() => {
+    console.log("server started successfully!");
+  })
+  .catch((err) => {
+    console.error("error starting server: ", err);
+  });
