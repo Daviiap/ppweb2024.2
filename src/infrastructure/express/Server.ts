048a2a9317cd6830a4376d4f68d5c341701b73c1
Daviiap <davii.sapiens@gmail.com>
2025-02-12 00:01:01 -0300

    docs: add Swagger documentation for Planning Poker API and integrate with Express server for improved API visibility

--- src/infrastructure/express/Server.ts
+++ src/infrastructure/express/Server.ts
@@ -5,15 +5,25 @@ import HttpServer, {
   routeConfig,
 } from "../../presentation/controllers/HttpServer";
 import ErrorGlobalMiddleware from "./middleware/ErrorMiddleware";
+import swaggerUi from "swagger-ui-express";
+import fs from "fs";
+import path from "path";
 
 export default class ExpressAdapter implements HttpServer {
   private readonly app: Express;
   private server: http.Server;
 
-  constructor(
-    private readonly errorMiddleware: ErrorGlobalMiddleware
-  ) {
+  constructor(private readonly errorMiddleware: ErrorGlobalMiddleware) {
     this.app = express();
+
+    const swaggerDocument = JSON.parse(
+      fs.readFileSync(
+        path.join(__dirname, "../../../docs/routes/swagger.json"),
+        "utf-8"
+      )
+    );
+    this.app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
     this.app.use(express.json());
     this.app.use(cors());
   }
